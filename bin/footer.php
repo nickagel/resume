c5eccebfe2a9c85d38fcae22398d3689cbac1fba
nickagel <nagel@uvm.edu>
2016-05-28 17:46:07 -0400

    footer with links, style etc

--- bin/footer.php
+++ bin/footer.php
@@ -1,3 +1,9 @@
-        <footer></footer>
+        <footer>
+            <ol>
+                <li><a id="git" href="//github.com/nickagel" target="_blank"></a></li>
+                <li><a id="insta" href="//www.instagram.com/nickagel" target="_blank"></a></li>
+                <li><a id="in" href="//www.linkedin.com/in/nickagel" target="_blank"></a></li>
+            </ol>
+        </footer>
     </body>
 </html>
