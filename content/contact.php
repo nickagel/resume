61902ebc6235b134945c684bc90c4a4c689455f6
nickagel <nagel@uvm.edu>
2016-05-28 15:23:58 -0400

    contact form setup and styling

--- content/contact.php
+++ content/contact.php
@@ -1,9 +1,21 @@
 <article id="contact" class="parent">
     <article class="child">
         <h1>Contact Me</h1>
-        <h2>Feel free to leave a message</h2>
     </article>
     <aside id="iframe">
         <iframe id="location" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d91075.2762749927!2d-73.29685984759256!3d44.492568893724226!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4cca7a55b69b55e5%3A0xc35fe519720e498e!2sBurlington%2C+VT!5e0!3m2!1sen!2sus!4v1463526299094"></iframe>
     </aside>
+    <form>
+        <fieldset>
+            <input placeholder="Name" type="text" name="name" value="<?php echo $name; ?>">
+            <input placeholder="Email" type="email" name="email" value="<?php echo $email; ?>">
+        </fieldset>
+        <fieldset>
+            <textarea placeholder="Message..." name="comments"></textarea>
+        </fieldset>
+        <fieldset>
+            	<input type="submit" name="submit" value="submit" class="button">
+        </fieldset>
+    </textarea>
+    </form>
 </article>
