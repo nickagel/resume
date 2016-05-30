0884ff4effe25b16cf3e7e691b7ba639da7e4968
nickagel <nagel@uvm.edu>
2016-05-30 16:18:20 -0400

    Worked on timeline
    -icon images
    -links
    -hover
    -dashed line
    -padding

--- content/about.php
+++ content/about.php
@@ -3,7 +3,10 @@
 <span>
     <img src="./images/placeholder.png">
     <h3>Nick Agel<h3>
-    <h4>Software Engineer & Web Developer</h4>
+    <h4><span class="blockWord">Software Engineer</span> & <span class="blockWord">Web Developer</span></h4>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent pulvinar eget nibh non faucibus. Mauris mollis ex vitae purus auctor, ac efficitur arcu ornare. Vestibulum sit amet odio erat. Etiam in magna porta nunc dignissim bibendum. Duis tempus sapien fringilla ipsum euismod, et varius est interdum. Nullam aliquam justo sed nisi aliquet, id commodo nisi malesuada. Morbi sed lacinia sapien. Praesent placerat magna quis lacus euismod, nec luctus risus pharetra.
+    </p>
     <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent pulvinar eget nibh non faucibus. Mauris mollis ex vitae purus auctor, ac efficitur arcu ornare. Vestibulum sit amet odio erat. Etiam in magna porta nunc dignissim bibendum. Duis tempus sapien fringilla ipsum euismod, et varius est interdum. Nullam aliquam justo sed nisi aliquet, id commodo nisi malesuada. Morbi sed lacinia sapien. Praesent placerat magna quis lacus euismod, nec luctus risus pharetra.
     </p>
