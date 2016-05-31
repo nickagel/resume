bf69b87f1c8d596579e53cd8ea8d6c809fab51c1
nickagel <nagel@uvm.edu>
2016-05-30 23:13:22 -0400

    fixed hover on portfolio content

--- content/portfolio.php
+++ content/portfolio.php
@@ -2,8 +2,8 @@
     $portfolio = array(
         array("uvmTheatre.png", "UVM Theatre", "//github.com/nickagel/UVM.Theatre", "#34495e"),
         array("birthdayCalculator.png", "Birthday Calculator", "//github.com/nickagel/Birthday-Calculator", "#ffb02f"),
-        array("myWebGrocer.png", "MyWebGrocer CONFIDENTIAL", "", "#006D81"),
-        array("resume.png", "Portfolio", "//github.com/nickagel/Resume", "#e67e22"),
+        array("myWebGrocer.png", "MyWebGrocer CONFIDENTIAL", "javascript:void(0);", "#006D81"),
+        array("resume.png", "Portfolio Site", "//github.com/nickagel/Resume", "#e67e22"),
         array("sorryStats.png", "Game of Sorry Stats", "//github.com/nickagel/Stats-Sorry", "#0085A1"),
         array("speechTherapyGames.png", "Speech Therapy Games", "//github.com/nickagel/Speech_Therapy_Games", "#bdc3c7"),
         array("uvmCEMS.png", "UVM CEMS", "//www.uvm.edu/~cems/", "#ecf0f1"),
