2a574be69dbf8f24db0b09a55d21e64005718e17
nickagel <nagel@uvm.edu>
2016-06-03 01:07:22 -0400

    adding scripts and fixing google font used

--- bin/top.php
+++ bin/top.php
@@ -6,11 +6,23 @@
         <link rel="stylesheet" href="./css/style.css">
         <link rel="stylesheet" href="./css/timeline.css">
         <link rel="stylesheet" href="./css/svg.css">
-        <link href='//fonts.googleapis.com/css?family=Roboto:300' rel='stylesheet' type='text/css'>
-        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
-        <script type="text/javascript" src="https://rawgit.com/imakewebthings/waypoints/master/lib/jquery.waypoints.min.js"></script>
-        <script type="text/javascript" src="https://rawgit.com/imakewebthings/waypoints/master/lib/shortcuts/inview.js"></script>
-        <script src="./scripts/timeline.js"></script>
-        <script src="./scripts/resume.js"></script>
+        <script type="text/javascript" src="./scripts/jquery-1.12.4.min.js"></script>
+        <script type="text/javascript" src="./scripts/waypoints.min.js"></script>
+        <script type="text/javascript" src="./scripts/inview.js"></script>
+        <script type="text/javascript" src="./scripts/timeline.js"></script>
+        <script type="text/javascript" src="./scripts/resume.js"></script>
+        <script type="text/javascript">
+            (function() {
+                var link_element = document.createElement("link"),
+                    s = document.getElementsByTagName("script")[0];
+                if (window.location.protocol !== "http:" && window.location.protocol !== "https:") {
+                    link_element.href = "http:";
+                }
+                link_element.href += "//fonts.googleapis.com/css?family=Roboto:100italic,100,300italic,300,400italic,400,500italic,500,700italic,700,900italic,900";
+                link_element.rel = "stylesheet";
+                link_element.type = "text/css";
+                s.parentNode.insertBefore(link_element, s);
+            })();
+        </script>
     </head>
     <body>
