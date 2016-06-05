1d97ef94998543168f0479ff9579d22246f72e5c
nickagel <nagel@uvm.edu>
2016-06-04 20:39:09 -0400

    convert google font file to js file

--- bin/top.php
+++ bin/top.php
@@ -1,5 +1,4 @@
 <!DOCTYPE html>
-    <html>
     <head>
         <title>Nick Agel</title>
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
@@ -12,18 +11,6 @@
         <script type="text/javascript" src="./js/timeline.js"></script>
         <script type="text/javascript" src="./js/resume.js"></script>
         <script type="text/javascript" src="./js/angular-1.5.6.min.js"></script>
-        <script type="text/javascript">
-            (function() {
-                var link_element = document.createElement("link"),
-                    s = document.getElementsByTagName("script")[0];
-                if (window.location.protocol !== "http:" && window.location.protocol !== "https:") {
-                    link_element.href = "http:";
-                }
-                link_element.href += "//fonts.googleapis.com/css?family=Roboto:100italic,100,300italic,300,400italic,400,500italic,500,700italic,700,900italic,900";
-                link_element.rel = "stylesheet";
-                link_element.type = "text/css";
-                s.parentNode.insertBefore(link_element, s);
-            })();
-        </script>
+        <script type="text/javascript" src="./js/roboto.google.font.js"></script>
     </head>
     <body ng-app="dataApp" ng-controller="dataCtrl">
