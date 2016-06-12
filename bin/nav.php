4ab8777810edba2ec3fb735fb430435299321483
nickagel <nagel@uvm.edu>
2016-06-12 19:31:56 -0400

    navigation update, placeholder for logo

--- bin/nav.php
+++ bin/nav.php
@@ -1,10 +1,11 @@
 <nav class="clearfix">
     <ul class="clearfix">
+        <li><a>Nick Agel</a></li>
         <li><a href="#about">About</a></li>
         <li><a href="#skills">Skills</a></li>
         <li><a href="#career">Career</a></li>
         <li><a href="#portfolio">Portfolio</a></li>
         <li><a href="#contact">Contact</a></li>
     </ul>
-    <a href="#" id="pull">Menu</a>
+    <a href="#" id="pull">Nick Agel</a>
 </nav>
