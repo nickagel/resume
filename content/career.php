b4a5008f3e49935fd826c6614e66b29cae1111ca
nickagel <nagel@uvm.edu>
2016-06-12 20:19:42 -0400

    Adjusted career content + added career begin

--- content/career.php
+++ content/career.php
@@ -1,8 +1,8 @@
 <h1>Career Experience</h1>
 <div id="timeline">
     <div ng-repeat="y in content.career"  emit-last-repeater-element-timeline>
-        <h3>{{y.company}}</h3>
-        <h4>{{y.title}}</h4>
+        <h3>{{y.title}}</h3>
+        <h4>{{y.company}}</h4>
         <p>{{y.description}}</p>
         <h5>{{y.date}}</h5>
     </div>
