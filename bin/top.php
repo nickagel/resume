c7f95acd4a9f1d149b800018ce8e240ed67ad09e
nickagel <nagel@uvm.edu>
2016-06-22 23:36:21 -0400

    remove no script because it broke the site

--- bin/top.php
+++ bin/top.php
@@ -4,5 +4,5 @@
         <title>Nick Agel</title>
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
           
-<script>var delayStyleSheet=function(){var e=document.createElement("link");e.rel="stylesheet",e.href="style.min.css";var t=document.getElementsByTagName("head")[0];t.parentNode.insertBefore(e,t)},request=requestAnimationFrame||mozRequestAnimationFrame||webkitRequestAnimationFrame||msRequestAnimationFrame;request?request(delayStyleSheet):window.addEventListener("load",delayStyleSheet);</script><noscript><link rel="stylesheet" href="./style.min.css"></noscript>
+<script>var delayStyleSheet=function(){var e=document.createElement("link");e.rel="stylesheet",e.href="style.min.css";var t=document.getElementsByTagName("head")[0];t.parentNode.insertBefore(e,t)},request=requestAnimationFrame||mozRequestAnimationFrame||webkitRequestAnimationFrame||msRequestAnimationFrame;request?request(delayStyleSheet):window.addEventListener("load",delayStyleSheet);</script>
 <style>  
