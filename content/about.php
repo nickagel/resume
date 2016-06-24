63bc154f58d4ac8dd730051518b4a350320a651d
nickagel <nagel@uvm.edu>
2016-06-23 23:54:41 -0400

    change about style

--- content/about.php
+++ content/about.php
@@ -5,19 +5,19 @@
 <span class="content">
         <img ng-src="./images/nickagel.jpg">
         <h2 class="rw-sentence">
-            <span><b class="bold">Hello.</b> My name is <b class="bold">Nick</b>.</span><br>
+            <span><b class="bold">Hello.</b> My name is <b class="bold">Nick.</b></span><br>
             <span id="aboutHello">I am a</span>					
             <div class="rw-words rw-words-1">
-                <span>Software Engineer</span>
-                <span>Web Developer</span>
-                <span>Photographer</span>
-                <span>Freelancer</span>
-                <span>Outdoor Enthusiast</span>
-                <span>Traveler</span>
+                <span>Software Engineer.</span>
+                <span>Web Developer.</span>
+                <span>Photographer.</span>
+                <span>Freelancer.</span>
+                <span>Outdoor Enthusiast.</span>
+                <span>Traveler.</span>
             </div>
             <br />
             <span>I am <b class="bold">passionate</b> about Web API &amp; UI design. </span>
-            <span>When I am <b class="bold">not coding</b>, I can be found <b class="bold">exploring</b> the great outdoors</span>
+            <span>When I am <b class="bold">not coding</b>, I can be found <b class="bold">exploring</b> the great outdoors.</span>
         </h2>
 </span>
 </article>
