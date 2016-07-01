a67ffd6218cabf4ca81f0621351927a861aae611
nickagel <nagel@uvm.edu>
2016-07-01 01:49:56 -0400

    contact form
    TODO: more validation and styling

--- content/contact.php
+++ content/contact.php
@@ -4,12 +4,12 @@
 <aside id="overlay" class="map">
 <iframe id="map"  class="scrolloff" src="" data-src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d91075.2762749927!2d-73.29685984759256!3d44.492568893724226!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4cca7a55b69b55e5%3A0xc35fe519720e498e!2sBurlington%2C+VT!5e0!3m2!1sen!2sus!4v1463526299094"></iframe>
 </aside>
-<form>
-    <legend id="message">Send Me a Message</legend>
-    <input placeholder="Name" type="text" name="name" autocomplete="off" required>
-    <input type="email" placeholder="Email" type="email" name="email" autocomplete="off" required>
-    <textarea placeholder="Message..." name="comments" type="text" autocomplete="off" required></textarea>
-    <input type="submit" value="Submit" class="button">
+<form id="ajax-contact" method="post" action="mailer.php">
+    <legend>Send Me a Message</legend>
+    <input placeholder="Name" type="text" type="text" id="name" name="name"  autocomplete="off" required>
+    <input placeholder="Email" type="email" id="email" name="email" autocomplete="off" required>
+    <textarea placeholder="Message..." id="message" name="message" type="text" autocomplete="off" required></textarea>
+    <button type="submit" value="Submit" class="button">SUBMIT</button><span id="form-messages"></span>
 </form>
 
 </article>
