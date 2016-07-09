8b953bc6962a429fe9cb40d7cd58d8c04022d468
nickagel <nagel@uvm.edu>
2016-07-08 22:54:12 -0400

    support no javascript & no requestAnimationFrame browsers. Minor fix .net keyframes onload, styled no script site

--- content/layover.php
+++ content/layover.php
@@ -1,1 +1,11 @@
 <div id="loading" class="overlayFadeIn"><figure class="loader"><figcaption class="pulsate">LOADING...</figcaption> </figure></div>
+
+<noscript>
+<div style="z-index:9000;" id="loading" class="overlayFadeIn"><figure class="loader"><figcaption style="font-size:150%;padding:20px;">
+JavaScript must be enabled in order for you to visit Nick Agel's site in standard view. 
+However, it seems JavaScript is either disabled or not supported by your browser. 
+To use standard view, enable JavaScript by changing your browser options, then try again.
+To use Nick Agel's basic HTML view, which does not require JavaScript,
+<a style="color: white;font-size: inherit;font-weight: inherit;text-decoration: none;" href="http://nagel.w3.uvm.edu/a/noscript/">click here</a>.
+</figcaption></figure></div>
+</noscript>
