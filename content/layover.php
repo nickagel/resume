d0ca93514f85597bdbda80adb21f7d99712dd6a2
nickagel <nagel@uvm.edu>
2016-07-17 13:53:14 -0400

    Minor styling adjustments for main view + seo

--- content/layover.php
+++ content/layover.php
@@ -2,10 +2,10 @@
 
 <noscript>
 <div style="z-index:9000;" id="loading" class="overlayFadeIn"><figure class="loader"><figcaption style="width:100%;font-size:150%;padding:20px;">
-JavaScript must be enabled in order for you to visit Nick Agel's site in standard view. 
+JavaScript must be enabled in order for you to visit this site in standard view. 
 However, it seems JavaScript is either disabled or not supported by your browser. 
 To use standard view, enable JavaScript by changing your browser options, then try again.
-To use Nick Agel's basic HTML view, which does not require JavaScript,
+To use basic HTML view, which does not require JavaScript,
 <a style="color: white;font-size: inherit;font-weight: inherit;text-decoration: none;" href="http://nickagel.com/noscript/">click here</a>.
 </figcaption></figure></div>
 </noscript>
