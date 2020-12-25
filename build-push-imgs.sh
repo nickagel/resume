7acc5ff43934da39b37c30e3a83d7f0c6795d619
Nick Agel <nagel@uvm.edu>
2020-12-24 20:34:32 -0600

    release scripts circa 2017

---
+++ build-push-imgs.sh
@@ -0,0 +1,3 @@
+gulp img
+ssh nagel@nickagel.com:/var/www/html 'mkdir -p www-root/a/images/'
+scp images/* nagel@nickagel.com:/var/www/nickagel.com/public_html/images
---
+++ build-push-main.sh
@@ -0,0 +1,2 @@
+gulp build
+scp -P 22 -r sitemap.xml robots.txt data.json .htaccess noscript mailer.php index.html scripts.min.js style.min.css nagel@nickagel.com:/var/www/nickagel.com/public_html
